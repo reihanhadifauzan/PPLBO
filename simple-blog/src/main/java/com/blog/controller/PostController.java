67b70817b2ed807e0af7cc548fc561e67094353d
Reihan Hadi Fauzan <132528145+reihanhadifauzan@users.noreply.github.com>
2024-02-22 11:20:44 +0700

    Update PostController.java

--- simple-blog/src/main/java/com/blog/controller/PostController.java
+++ simple-blog/src/main/java/com/blog/controller/PostController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blog.vo.Post;
-@Controller
+@RestController
 public class PostController {
 
     @GetMapping("/post")
