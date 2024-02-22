f8ec2e2347dcbb491c7c84e2486c178cba3bbe55
ReihanHadiF <reihan.hadi.tif421@polban.ac.id>
2024-02-22 10:16:44 +0700

    Initial Commit

---
+++ simple-blog/src/main/java/com/blog/controller/PostController.java
@@ -0,0 +1,17 @@
+package com.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blog.vo.Post;
+@Controller
+public class PostController {
+
+    @GetMapping("/post")
+    public Post getPost() {
+        Post post = new Post(1L, "Mike", "Mike's Post", "Welcome to My Blog");
+        return post;
+    }
+    
+}
